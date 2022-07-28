ceff0e2f4d3421ab972f02964691ccbfcfa944f3
Michael Legere <michaellegere@macbook-pro-8.bc.edu>
2022-07-28 13:48:06 -0400

    Attempt to add proper routing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 function App() {
 
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <>
             <NavBar />
             <Routes>
